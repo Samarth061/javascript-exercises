a60376531cf1f2a504c104b6b64b71ab55fdd005
Samarth061 <140432072+Samarth061@users.noreply.github.com>
2024-08-26 17:32:24 -0500

    All test pass for 05_sumAll, edit .js file  and .spec.js file

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if((num1 || num2) < 0 || !Number.isInteger(num1) || !Number.isInteger(num2))
+    {
+        return "ERROR";
+    }
+
+    const max = Math.max(num1, num2);
+    const min = Math.min(num1, num2);
+    let sum = 0;
+
+    for(let i = min; i <= max; i++)
+    {
+        sum += i;
+    }
+    return sum;
+
 
 };
 
