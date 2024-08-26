104f5ae480f472483cf2cd03d39c6c7d2d2a32aa
Samarth061 <140432072+Samarth061@users.noreply.github.com>
2024-08-26 17:49:53 -0500

    All test pass for 07_tempConversion, edit .js file and .spec.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  let Celsius = (Fahrenheit - 32) * 5/9;
+  return notInteger(Celsius);
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  let Fahrenheit = (Celsius * 9/5) + 32;
+  return notInteger(Fahrenheit);
+  
 };
 
+function notInteger(num){
+  if (!Number.isInteger(num))
+  {
+    return parseFloat(num.toFixed(1));
+  }
+  return num;
+}
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
