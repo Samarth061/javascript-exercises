4cd275a699f2ab9773ca0bddaf404c8dce9bab79
Samarth061 <140432072+Samarth061@users.noreply.github.com>
2024-08-26 16:33:12 -0500

    All test pass for 04_removeFromArray, edit .js  file, .spec.js file

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const arr = args[0];
+    
+    for(let i = 1; i < args.length; i++)
+    {
+        let num = args[i];
+        if(arr.includes(num))
+        {
+            let index = arr.indexOf(num)
+            arr.splice(index,1);
+            i--;
+        }
+        else{
+            continue;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
