fae0fa078c8ac723f1ec0c75a420f09b89d82068
Samarth061 <140432072+Samarth061@users.noreply.github.com>
2024-09-01 21:23:11 -0500

    All test pass for 10_fibonacci, edit .js file and .spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(numb) {
+    let intNumb = Number(numb);
+    if(intNumb < 0)
+    {
+        return "OOPS";
+    }
+    else if(intNumb == 0)
+    {
+        return 0;
+    }
+    else if(intNumb == 1){
+        return 1;
+    }
 
+    return fibonacci(intNumb-1) + fibonacci(intNumb-2);
 };
 
 // Do not edit below this line
