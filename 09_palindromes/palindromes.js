f90f9cd239a223a7be364035383763af3c111f5a
Samarth061 <140432072+Samarth061@users.noreply.github.com>
2024-09-01 21:12:59 -0500

    All test pass for 09_palindromes, edit .js file and .spec.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanedStr = str.replace(/[ .,!?]/g, "");
+    let lCase = cleanedStr.toLowerCase();
+    let j = lCase.length - 1;
+    console.log(lCase);
+    for( let i = 0; i < j; i++)
+    {
+        if(lCase.charAt(i) != lCase.charAt(j))
+        {
+            return false;
+        }
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
