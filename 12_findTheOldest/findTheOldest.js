f24a691eae2ebf877eac7d04797e95c3ea5b411a
Samarth061 <140432072+Samarth061@users.noreply.github.com>
2024-09-01 22:11:24 -0500

    All test pass for 12_findTheOldest, edit .js file and .spec.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(person) {
+    let oldest = person.sort((a,b)=> {
+        if( a.yearOfDeath == undefined )
+        {
+            const today = new Date();
+            a.yearOfDeath =  today.getFullYear();
+        } 
+        else if(b.yearOfDeath == undefined)
+        {
+            const today = new Date();
+            b.yearOfDeath =  today.getFullYear();
+        }
+        let aAge = a.yearOfBirth - a.yearOfDeath;
+        let bAge = b.yearOfBirth - b.yearOfDeath;
+        return aAge > bAge ? 1 : -1;
+    });
+    console.table(oldest);
+    return oldest[0];
 
 };
 
