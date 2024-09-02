160d6d087d0338121b5ebcc6b1596348efe7b003
Samarth061 <140432072+Samarth061@users.noreply.github.com>
2024-09-01 21:53:22 -0500

    All test pass for 11_getTheTitles, edit .js file and .spec.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,6 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    return books.map((books) => books.title);
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
